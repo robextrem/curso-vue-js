f642dca3fc4143a5165bc4bad0997d399f0f256c
Roberto Romero <roberto@arrenda.mx>
2023-08-14 11:17:47 -0600

    Hello world

---
+++ hello-world/src/App.vue
@@ -0,0 +1,31 @@
+<script>
+  export default{
+    data: function(){
+      return {
+        nombre: "Ulises",
+        pais: "México",
+        color: "rojo"
+      }
+    },
+    methods:{
+      cambiarNombre: function(){
+        this.nombre = "Zabdiel"
+      }
+    }
+  }
+</script>
+<template>
+  <div>
+    <h1>Hola mundo, {{ nombre }}!</h1>
+    <p :class="color">Bienvenido al curso desde {{ pais }}</p>
+    <button @click="cambiarNombre">Zabdiel</button>
+  </div>
+</template>
+<style>
+  .rojo{
+    background-color:red
+  }
+  .azul{
+    background-color: blue;
+  }
+</style>
