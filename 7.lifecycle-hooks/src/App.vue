b866e9c71a5ff6176b091d70a954e3a8232ddf1c
Roberto Romero <roberto@arrenda.mx>
2023-08-15 12:12:35 -0600

    ciclo de vida

---
+++ 7.lifecycle-hooks/src/App.vue
@@ -0,0 +1,29 @@
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+export default{
+  components:{
+    HelloWorld
+  },
+  data: function(){
+    return{
+      mensaje: "Otra vez",
+      alumnos:[]
+    }
+  },
+  mounted:function(){
+    console.log("El componente se encuentra montado")
+  },
+  beforeMount:function(){
+    this.alumnos = ['Marcelino','Ana','Zabdiel','Ulises']
+    console.log("El componente se encuentra antes de ser montado")
+  },
+  updated:function(){
+    console.log("El componente se ha actualizado")
+  }
+}
+</script>
+<template>
+  <h1>Componente</h1>
+  <HelloWorld v-if="mensaje"></HelloWorld>
+  <h1 :key="i" v-for="(alumno, i) in alumnos">Hola mundo!! {{ alumno }}</h1>
+</template>
---
+++ 7.lifecycle-hooks/src/components/HelloWorld.vue
@@ -0,0 +1,14 @@
+<script>
+export default{
+  beforeUnmount: function(){
+    console.log("El componente se va a destruir")
+  },
+  unmounted:function(){
+    alert("Se destruyo componente")
+    console.log("El componente se ha destruido")
+  }
+}
+</script>
+<template>
+  <h1>Componente para destruir el  mundo</h1>
+</template>
